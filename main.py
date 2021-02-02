c648324dc268854dd6317b0ee21c944b1ce1970a
Abraham Sesay <abrahamsesay96@gmail.com>
2021-02-02 09:54:16 -0600

    add title

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # ask the user what town they grew up in
-
+print("Welcome to the band name generator:")
 town = input("What town did you grow up in:\n")
 
 pet = input("What is the name of your a pet:\n")
