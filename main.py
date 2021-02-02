5d1e135915013d2ff51711860a1305a37c3407f3
Abraham Sesay <abrahamsesay96@gmail.com>
2021-02-02 09:50:37 -0600

    init commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# ask the user what town they grew up in
+
+town = input("What town did you grow up in:\n")
+
+pet = input("What is the name of your a pet:\n")
+
+band = (town + pet +"\n")
+
+print(band)
